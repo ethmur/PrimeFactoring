d4888ddd1afbaa6238467e6545f7c34e11c97d59
Ethan Murad <etmuop@gmail.com>
2016-04-24 08:58:34 -0400

    created project skeleton

---
+++ src/BigNumber.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+
+public class BigNumber implements Comparable<BigNumber> {
+	
+	ArrayList<Byte> digits;
+	
+	public BigNumber(int starting_value) {
+		
+	}
+	
+	public void addBy(BigNumber num) {
+		
+	}
+	
+	public void subtractBy(BigNumber num) {
+		
+	}
+	
+	public void multiplyBy(int num) {
+		
+	}
+	
+	public void modWith(BigNumber num) {
+	}
+	
+	@Override
+	public int compareTo(BigNumber num) {
+		return 0;
+	}
+	
+	@Override
+	public String toString() {
+		return digits.toString();
+	}
+
+}
